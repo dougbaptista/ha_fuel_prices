785fd710ae664f3857af04146bf4e15b75a226ae
Douglas <doug.baptista@gmail.com>
2025-01-24 19:19:12 -0300

    sync_forward_entry_setup está obsoleto e será descontinuado em uma versão futura do Home Assistant. A recomendação é substituir por async_forward_entry_setups

--- custom_components/ha_fuel_prices/__init__.py
+++ custom_components/ha_fuel_prices/__init__.py
@@ -1,22 +1,22 @@
-from homeassistant.core import HomeAssistant
 from homeassistant.config_entries import ConfigEntry
-
+from homeassistant.core import HomeAssistant
 from .const import DOMAIN
 
-async def async_setup(hass: HomeAssistant, config: dict):
-    """Setup básico para permitir a inicialização do domínio."""
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Configure a entrada do Fuel Prices usando Config Flow."""
     hass.data.setdefault(DOMAIN, {})
-    return True
-
-async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
-    """Configura a integração a partir da entrada da interface de configuração."""
     hass.data[DOMAIN][entry.entry_id] = entry.data
 
-    await hass.config_entries.async_forward_entry_setup(entry, "sensor")
+    # Substituindo o método obsoleto por async_forward_entry_setups
+    await hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
+
     return True
 
-async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
-    """Descarrega a integração."""
-    await hass.config_entries.async_forward_entry_unload(entry, "sensor")
-    hass.data[DOMAIN].pop(entry.entry_id)
-    return True
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Descarrega a entrada da integração."""
+    unload_ok = await hass.config_entries.async_unload_platforms(entry, ["sensor"])
+
+    if unload_ok:
+        hass.data[DOMAIN].pop(entry.entry_id)
+
+    return unload_ok
