a0c95c67162fdcbe5cdf667921452b6bc7908e40
Douglas <doug.baptista@gmail.com>
2025-01-24 18:47:40 -0300

    ajustes

--- custom_components/ha_fuel_prices/config_flow.py
+++ custom_components/ha_fuel_prices/config_flow.py
@@ -4,15 +4,16 @@ import voluptuous as vol
 from .const import DOMAIN
 
 class FuelPricesConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
-    """Gerencia o fluxo de configuração da integração."""
+    """Configuração do fluxo de integração para preços de combustíveis."""
 
     VERSION = 1
 
     async def async_step_user(self, user_input=None):
-        """Passo inicial para a configuração do usuário."""
+        """Passo inicial da configuração do usuário."""
         if user_input is not None:
             return self.async_create_entry(title="Fuel Prices", data=user_input)
 
+        # Esquema de configuração do usuário
         schema = vol.Schema({
             vol.Required("state", default="SANTA CATARINA"): str,
             vol.Required("city", default="TUBARAO"): str,
